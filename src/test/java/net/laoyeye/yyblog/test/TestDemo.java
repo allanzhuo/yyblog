f98b50d0d8fc85a727652ec68f817dc3e41abfcd
allanzhuo <23962957@qq.com>
2018-07-21 12:46:05 +0800

    单元测试

---
+++ src/test/java/net/laoyeye/yyblog/test/TestDemo.java
@@ -0,0 +1,13 @@
+package net.laoyeye.yyblog.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDemo {
+
+}
