08082fe00be66599ffcdde1993053a9260c0ee70
allanzhuo <23962957@qq.com>
2018-08-03 18:55:10 +0800

    文章收藏

---
+++ src/main/java/net/laoyeye/yyblog/model/CollectDO.java
@@ -0,0 +1,85 @@
+package net.laoyeye.yyblog.model;
+
+import java.util.Date;
+
+public class CollectDO {
+    private Long id;
+
+    private Long userId;
+
+    private Long articleId;
+
+    private Boolean enable;
+
+    private Long createUser;
+
+    private Date createTime;
+
+    private Long updateUser;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
