49295ad2a72b5e3f14d3938bc95da86c72dd5f12
allanzhuo <23962957@qq.com>
2018-08-05 18:59:34 +0800

    user实现

--- src/main/java/net/laoyeye/yyblog/service/impl/UserServiceImpl.java
+++ src/main/java/net/laoyeye/yyblog/service/impl/UserServiceImpl.java
@@ -57,4 +57,10 @@ public class UserServiceImpl implements UserService{
         
         return userMapper.getNicknameById(id);
     }
+
+	@Override
+	public UserDO getUserById(long id) {
+
+		return userMapper.getUserById(id);
+	}
 }
