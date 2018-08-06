892277f8cd33e43c949ccb6accaaee00ddbf957e
allanzhuo <23962957@qq.com>
2018-08-06 19:00:09 +0800

    删除无用代码

--- src/main/java/net/laoyeye/yyblog/service/QqLoginService.java
+++
@@ -1,8 +0,0 @@
-package net.laoyeye.yyblog.service;
-
-import javax.servlet.http.HttpServletRequest;
-import net.laoyeye.yyblog.common.YYBlogResult;
-
-public interface QqLoginService {
-    YYBlogResult login(HttpServletRequest request);
-}
